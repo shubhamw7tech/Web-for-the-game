ab6fce5e2248988c846d39619c88aae3c30d5f38
shubhamw7tech <shubham440ff@gmail.com>
2026-07-28 08:13:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("btn")
+
+button.addEventListener("click", function() {
+	window.location.href = "https://shubhamw7tech.github.io/2.5d-game/";
+})
+
+
+//alert("welcome")
+//console.log(innerWidth)
+//console.log(innerHeight)
